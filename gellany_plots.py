5d27ad5ce020f106c8f805958c1d1a2fc6250e2b
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-03 18:45:16 +0000

    Create gellany_plots.py

---
+++ gellany_plots.py
@@ -0,0 +1,9 @@
+import pickle
+import tldextract
+import argparse
+import json
+
+class backbone():
+    
+# init method or constructor
+    def __init__(self, var1= None, title1 = '', var2 = None, title2 = '', transformed = False, read_file = None, hue= None, type =None, label = None, drop = None, encode = None):
