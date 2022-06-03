5aab335e06fcb7e2221c6bc4262dbcbe948f134e
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-03 19:46:15 +0000

    Update gellany_dga.py

--- gellany_dga.py
+++ gellany_dga.py
@@ -10,19 +10,7 @@ class dga_inspector():
 # init method or constructor
     def __init__(self, domain = None, pki = None,var1= None, title1 = '', var2 = None, title2 = '', transformed = False, read_file = None, hue= None, type =None, label = None, drop = None, encode = None):
 
-                          self.data = data
-                          self.var1 = var1
-                          self.title1 = title1
-                          self.var2 = var2
-                          self.title2 = title2
-                          self.transformed = transformed
-                          self.read_file = str(read_file)
-                          self.hue = hue
-                          self.type = type
-                          self.df_test = df_test
-                          self.label = label
-                          self.drop = drop
-                          self.encode = encode
+                          
                           self.pki = pki
                           self.domain = domain
           
