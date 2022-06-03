92bacf8c4644109f5805d826ae20574dbe9b0c44
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-03 19:06:21 +0000

    Update gellany_plots.py

--- gellany_plots.py
+++ gellany_plots.py
@@ -7,3 +7,24 @@ class backbone():
     
 # init method or constructor
     def __init__(self, var1= None, title1 = '', var2 = None, title2 = '', transformed = False, read_file = None, hue= None, type =None, label = None, drop = None, encode = None):
+
+                          self.data = data
+                          self.var1 = var1
+                          self.title1 = title1
+                          self.var2 = var2
+                          self.title2 = title2
+                          self.transformed = transformed
+                          self.read_file = str(read_file)
+                          self.hue = hue
+                          self.type = type
+                          self.df_test = df_test
+                          self.label = label
+                          self.drop = drop
+                          self.encode = encode
+          
+                        
+                                            
+                                              
+          
+                  
+          
